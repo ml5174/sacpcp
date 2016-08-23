65ee964c697dfcef662e49ad04eae5ba47a0faaf
Lopez, Mauricio (ml5174) <ml5174@att.com>
2016-08-23 15:19:50 -0400

    started registration

--- app/pages/register/register.ts
+++ app/pages/register/register.ts
@@ -1,14 +1,35 @@
-import {Component} from '@angular/core';
-
+import {Component} from '@angular/core'
+import {LoginServices} from '../../service/login';
+import {NavController, Nav} from 'ionic-angular';
 
 @Component({
-  templateUrl: 'build/pages/register/register.html'
+  templateUrl: 'build/pages/register/register.html',
+  providers: [LoginServices]
 })
 export class RegisterPage {
-  constructor() {
+  username: string = '';
+  password1: string = '';
+  password2: string = '';
+  email: string = '';
+  constructor(private nav: NavController,
+              private loginServices: LoginServices) {
 
   }
   register() {
     
+    let register = {
+      username: this.username,
+      password1: this.password1,
+      password2: this.password2,
+      email: this.email
+    }
+    this.loginServices.register(register)
+      .subscribe(
+          key => this.key = key, 
+          err => { 
+            console.log(err);
+            this.setError(err);
+          }),
+          val => this.val = val;
   }
 }
