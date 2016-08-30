f16d30aaa1d723831ab04f43702e757c17f22989
ml5174 <weolopez@ml5174.local>
2016-08-30 15:27:29 -0400

    added register user profile

--- app/pages/tabs/tabs.ts
+++ app/pages/tabs/tabs.ts
@@ -4,8 +4,10 @@ import {AwardsPage} from './awards/awards';
 import {DonatePage} from './donate/donate';
 import {VolunteerPage} from './volunteer/volunteer';
 import {NavController, Nav, NavParams} from 'ionic-angular';
+import {RegisterIndividualProfilePage} from '../register-individual-profile/register-individual-profile';
 import {LogonPage} from '../logon/logon';
 import {LoginServices} from '../../service/login';
+import {UserProfile} from '../../model/user-profile'
 
 @Component({
   templateUrl: 'build/pages/tabs/tabs.html',
@@ -17,18 +19,39 @@ export class TabsPage {
   private donatePage: any;
   private volunteerPage: any;
   private loginKey: string;
+  private profile: UserProfile;
 
   constructor(private nav: Nav, 
-              private navParams: NavParams) {
+              private navParams: NavParams,
+              private loginServices: LoginServices) {
                 
     this.homePage = HomePage;
     this.awardsPage = AwardsPage;
     this.donatePage = DonatePage;
     this.volunteerPage = VolunteerPage;
+
     this.loginKey = navParams.get('key');
-    if ( this.loginKey === undefined ) this.login();
   }
 
+  
+  ngOnInit(){
+    this.getUserProfile();
+  }
+  getUserProfile() {
+    this.loginServices
+        .getUser(this.loginKey).subscribe(
+                               profile => this.profile = profile, 
+                                err => {
+                                    console.log(err);
+                                    this.handleUserProfileError(err);
+                                });
+  }
+  handleUserProfileError(error) {
+    if (error.detail) {
+      if (error.detail==='Not found.') this.nav.push(RegisterIndividualProfilePage);
+      if (error.detail=== 'Authentication credentials were not provided.' ) this.login();
+    }
+  }
   login() {
     this.nav.push(LogonPage);
   }
--- app/service/login.ts
+++ app/service/login.ts
@@ -1,8 +1,10 @@
 import {Injectable} from '@angular/core';
-import {Http} from '@angular/http';
+import {Http, Headers} from '@angular/http';
 import {Observable} from 'rxjs/Rx';
 import {LOGIN_URI} from '../provider/config';
 import {REGISTER_URI} from '../provider/config';
+import {REGISTER_USER_URI} from '../provider/config';
+import {SERVER} from '../provider/config';
 
 @Injectable()
 export class LoginServices {
@@ -11,12 +13,26 @@ export class LoginServices {
     //    username: "testuser"
     //    password: "password123"
     login(body): Observable<any> {
-        return this.http.post(LOGIN_URI, body)
+        return this.http.post(SERVER+LOGIN_URI, body)
             .map(res => res.json())
             .catch((error: any) => Observable.throw(error.json() || 'Server error'));
     }
     register(body): Observable<any> {
-        return this.http.post(REGISTER_URI, body)
+        return this.http.put(SERVER+REGISTER_URI, body)
+            .map(res => res.json())
+            .catch((error: any) => Observable.throw(error.json() || 'Server error'));
+    }
+    updateUser(body): Observable<any> {
+        return this.http.post(SERVER+REGISTER_USER_URI, body)
+            .map(res => res.json())
+            .catch((error: any) => Observable.throw(error.json() || 'Server error'));
+    }
+    getUser(loginKey): Observable<any> {
+        let headers = new Headers();
+        if (loginKey !== undefined ) headers.append('Authorization', 'Token '+loginKey.key);
+        return this.http.get(SERVER+REGISTER_USER_URI, {
+                headers: headers
+                })
             .map(res => res.json())
             .catch((error: any) => Observable.throw(error.json() || 'Server error'));
     }
