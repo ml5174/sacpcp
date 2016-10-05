b52578328fee428fcb17c675e7782a9f376a2375
Defibaugh, Michael (md388s) <md388s@us.att.com>
2016-10-05 10:02:52 -0400

    Added backed locations call and model uncommented events call on home page

---
+++ app/model/locations.ts
@@ -0,0 +1,19 @@
+export class Locations {
+  id: number;
+  name: string;
+  description: string;
+  website: string;
+  maplink: string;
+  email: string;
+  mobilenumber: string;
+  address1: string;
+  address2: string;
+  city: string;
+  state: string;
+  zipcode: string;
+  contactmethod: string;
+  contactmethod_name: string;
+  email_validated: number;
+  phone_validated: number;
+  address_validated: number;
+}
