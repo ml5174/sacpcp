5adda2600124a1d6a7fc738cde5b9d9efd88a60a
md388s <MD388S@GACDTL01MD388S.ITServices.sbc.com>
2016-10-24 10:27:20 -0400

    no default image displayed if no image found search is AND delineated by spaces fixed bug resulting from clicking on events tab before event images were done loading

--- src/model/volunteer-event.ts
+++ src/model/volunteer-event.ts
@@ -2,16 +2,16 @@ import {EventImage} from './eventImage';
 
 export class VolunteerEvent {
   id: number;
-  title: string;
-  category: string;
-  description: string;
-  start: Date;
-  end: Date;
+  title: string = "";
+  category: string ="";
+  description: string = "";
+  start: Date = new Date();
+  end: Date = new Date();
   rule: string;
   end_recurring_period: string;
   location_id: string;
-  location_name: string;
-  created_by: string;
-  image: Array<EventImage>;
+  location_name: string = "";
+  created_by: string = "";
+  image: Array<EventImage> = [new EventImage()];
 
 }
