40f8073c186ca74bca730ea50182eecb62893c22
Daniel Fienberg <df6162@intl.att.com>
2016-11-01 15:25:01 +0200

    new deploy client script

--- deploy_client.sh
+++ deploy_client.sh
@@ -1,45 +1,38 @@
 #!/bin/bash
-export PATH=/usr/sbin:/usr/local/bin:/usr/bin:/bin
-set -x
-TIMESTAMP=$(date +%Y%m%d_%H%M)
-echo "See logs at ~/client/deploy_logs/deploy_${TIMESTAMP}.stderr"
-exec 1>~/client/deploy_logs/deploy_${TIMESTAMP}.stdout
-exec 2>~/client/deploy_logs/deploy_${TIMESTAMP}.stderr
 
-DEPLOY_DIR=~/client
-REPO_NAME='client_repo'
-RUN_DIR=~/client/client_site
-REPO_URL='https://git-codecommit.us-east-1.amazonaws.com/v1/repos/CPC_Client'
-#EMAIL_TO='salarmycpc_dev@list.att.com'
-#EMAIL_FROM='salarmycpc_dev@list.att.com'
-EMAIL_TO='df6162@intl.att.com'
-EMAIL_FROM='df6162@intl.att.com'
+ENV=$1
+VERSION=$2
 
-# Delete current repo
-cd ${DEPLOY_DIR}
-[ -d ${REPO_NAME} ] && rm -rf ${REPO_NAME}
-git clone ${REPO_URL} ${REPO_NAME}
+ENV_FILE=${ENV}.env
+DEPLOY_DIR=~/deploy
+S3_REGION='us-east-1'
+MANAGEMENT_BUCKET='salarmy-management'
+DEPLOY_BUCKET="salarmy-${ENV}-client"
 
-# Load NVM
-export NVM_DIR="/home/salarmy/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+function dir_setup {
+  [[ ! -d ${DEPLOY_DIR} ]] && mkdir -p ${DEPLOY_DIR}
+}
 
-cd ${DEPLOY_DIR}/${REPO_NAME}
-npm install -g ionic
-ionic build
-sudo ln -sf ${DEPLOY_DIR}/${REPO_NAME}/www/ /var/www/html
-curl -Ss localhost >> /dev/null
-ret=$?
-if [[ ${ret} -eq 0 ]] ; then
-  rm -rf ${RUN_DIR}/www
-  mv ${DEPLOY_DIR}/${REPO_NAME}/www ${RUN_DIR}/
-  sudo ln -sf ${RUN_DIR}/www/ /var/www/html
-  echo -e "Subject: Successfully deployed latest version of Salarmy Client on $(date)\n\r\
-  Body: Salarmy Client successfully deployed on $(date)." | \
-  sendmail -f ${EMAIL_FROM} ${EMAIL_TO}
-else
-  sudo ln -sf ${RUN_DIR}/www/ /var/www/html
-  echo -e "Subject: Failed to deploy latest version of Salarmy Client on $(date)\n\r\
-  Body: Salarmy Client failed to deploy on $(date)." | \
-  sendmail -f ${EMAIL_FROM} ${EMAIL_TO}
-fi
+function get_version {
+  # Get VERSION variable
+  cd ${DEPLOY_DIR}
+  aws s3 cp s3://${MANAGEMENT_BUCKET}/env/${ENV_FILE} . --region ${S3_REGION}
+  . ${ENV_FILE}
+}
+
+function deploy_site {
+  cd ${DEPLOY_DIR}
+  aws s3 cp s3://${MANAGEMENT_BUCKET}/client-packages/client-${VERSION}.tgz . --region ${S3_REGION}
+  tar -xf client-${VERSION}.tgz 
+  aws s3 sync www s3://${DEPLOY_BUCKET}
+  rm -f client-${VERSION}.tgz
+  rm -rf www
+}
+
+function main {
+  dir_setup
+  [[ -z ${VERSION} ]] && get_version
+  deploy_site
+}
+
+main
