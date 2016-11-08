80015af656af4e3f19631f268b77e90dce1f74a7
Defibaugh, Michael (md388s) <md388s@us.att.com>
2016-11-08 15:14:25 -0500

    moved events to seperate component

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,9 +1,6 @@
 import {Component} from '@angular/core';
 import {NavController, Nav} from 'ionic-angular';
-import {VolunteerEvent} from '../../model/volunteer-event';
 import {VolunteerEventsService} from '../../service/volunteer-events-service';
-import {EventImage} from '../../model/eventImage';
-import {Locations} from '../../model/locations';
 import {LoginPage} from '../login/login';
 import {TranslateService} from 'ng2-translate/ng2-translate';
 import { UserServices } from '../../service/user';
@@ -20,21 +17,8 @@ loop: true,
 pager: true, 
 autoplay: 3000};
 
-  search: boolean = false;
-  previousevents: boolean = true;
-  yourevents: boolean = true;
-  events: Array<VolunteerEvent> = [];
-  searchedEvents: Array<VolunteerEvent> = [];
-  maxEvents: Array<VolunteerEvent> = [];
-  minEvents: Array<VolunteerEvent> = [];
-  locations: Array<Locations> = [];
-  image: Array<EventImage>;
   selectedTab: string = "home";
   language: string = "en";
-  val: string ="";
-  values: Array<String>;
-  searching: Boolean = false;
-  noResults: Boolean = false;
 
   constructor(private navCtrl: NavController,
               private nav: Nav, 
@@ -42,96 +26,7 @@ autoplay: 3000};
               private translate: TranslateService,
               private userServices: UserServices
   ) {  }
-  ngOnInit(){
-    this.getEvents();
-  }
-  onCancel(event: any) {
-    this.search=false;
-  }
-  getItems(ev: any) {
-    if(ev.target.value == undefined){
-      ev.target.value = '';
-    }
-    this.searching = true;
-    this.noResults = false;
-    this.searchedEvents = this.events;
-    // set val to the value of the searchbar
-    this.val = ev.target.value;
-    this.val = this.val.trim();
-    this.val = this.val.toLowerCase();
-    this.values = this.val.split(" ");
-    if (this.val && this.val.trim() != '') {
-      for (var i = 0; i < this.values.length; ++i) {
-        this.searchedEvents = this.searchedEvents.filter((item) => {
-          return ((item.description.toLowerCase().indexOf(this.values[i].toLowerCase()) > -1) ||
-            (item.title.toLowerCase().indexOf(this.values[i].toLowerCase()) > -1) ||
-            (item.location_name !=null &&
-            (item.location_name.toLowerCase().indexOf(this.values[i].toLowerCase()) > -1)) ||
-            (item.location_address1 !=null &&
-            (item.location_address1.toLowerCase().indexOf(this.values[i].toLowerCase()) > -1)) ||
-            (item.location_city !=null &&
-            (item.location_city.toLowerCase().indexOf(this.values[i].toLowerCase()) > -1))
-            )});
 
-      }
-      if (this.searchedEvents.length==0){
-        this.noResults = true;
-      }
-    } else {
-      this.searching = false;
-    }
-  }
-  populateSearchedEvents(ev: VolunteerEvent[]){
-    this.events = ev;
-    this.searchedEvents = this.events;
-    for (let event of this.events) {
-     this.volunteerEventsService
-        .getEventImage(event.id).subscribe(
-                               image => {this.image = image;
-                                         event.image = this.image;
-                                         if(this.image.length==0){
-                                            this.image[0] = new EventImage();
-                                            event.image = this.image;};
-                                        }, 
-                                err => {
-                                    console.log(err);
-                                }, 
-                                () => this.searchedEvents = this.events);
-    }
-  }
-  getEvents() {
-    this.volunteerEventsService
-        .getVolunteerEvents().subscribe(
-                               event => this.events = event, 
-                                err => {
-                                    console.log(err);
-                                }, 
-                                () => {this.searchedEvents = this.events;
-                                       this.populateSearchedEvents(this.events);
-                                     });
-                                +    this.volunteerEventsService
-         .getLocations().subscribe(
-                                locations => this.locations = locations, 
-                                 err => {
-                                     console.log(err);
-                                 });
-  }
-  getEventsMax(maxTime) {
-     this.volunteerEventsService
-         .getVolunteerEventsMaxTime(maxTime).subscribe(
-                                events => this.maxEvents = events, 
-                                 err => {
-                                     console.log(err);
-                                 });
-  }
-  getEventsMin(minTime) {
-     this.volunteerEventsService
-         .getVolunteerEventsMinTime(minTime).subscribe(
-                                events => this.minEvents = events, 
-                                 err => {
-                                     console.log(err);
-                                 });
-  }
   changeLanguage() {
     // use navigator lang if English(en) or Spanish (es)
     var userLang = this.language; 
@@ -146,9 +41,5 @@ autoplay: 3000};
   noTabs() {
     this.nav.pop();
   }
-  whoamI(){
-    if (this.userServices.user.id){
-      console.log(this.userServices.user.id);
-    }
-  }
+
 }
