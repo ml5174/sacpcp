57e4ea13c190bf469763b417cb9c69c2b666a81e
Defibaugh, Michael (md388s) <md388s@us.att.com>
2017-02-02 20:55:24 -0500

    added constants

--- provider/eventConstants.ts
+++ provider/eventConstants.ts
@@ -16,7 +16,7 @@ export const EVENT_STATUS = {
         2: 'Cancelled'
 }
 export const SAMEDAY_RESTRICTION = {
-        0: 'none',
+        0: 'no',
         1: 'yes',
         2: 'no',
         3: 'instaff'
@@ -25,4 +25,15 @@ export const AGE_RESTRICTION = {
         0: 'none',
         1: '16 and over',
         2: 'under 16'
+}
+//yeah, this one probably needs some adjusting
+export const NOTIFICATION_SCHEDULE = {
+        0: 'none',
+        1: 'days',
+        7: 'weeks',
+        14: 'two-weeks'
+}
+export const NOTIFICATION_OPTIONS = {
+        0: 'no',
+        1: 'yes'
 }
