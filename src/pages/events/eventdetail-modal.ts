23a7b0b4c99ffddf3d0e67b82a5207f67ac7b9a0
pp4253 <pp4253@CACDTL06PP4253.ITServices.sbc.com>
2017-02-22 09:45:18 -0800

    update EventDetails

--- src/pages/events/eventdetail-modal.ts
+++ src/pages/events/eventdetail-modal.ts
@@ -128,7 +128,7 @@ export class EventDetailModal {
     {
         this.cancelEditDetails = toggle;
     }
-
+   // Update EventDetail
    updateEventDetails() {
         this.volunteerEventsService
             .updateEventDetails(this.eventDetail).subscribe(
