7228a952e8c77498321f195d1730f766462df4cc
Shine, Michael (ms8342) <ms8342@us.att.com>
2017-11-08 00:20:22 -0500

    added src/pages/mygroups containing placeholder for MyGroupsPage

---
+++ src/pages/mygroups/mygroups.ts
@@ -0,0 +1,91 @@
+import {Component, ViewChild} from '@angular/core'
+import {Observable} from 'rxjs/Rx';
+import {UserServices} from '../../lib/service/user';
+import {NavController,NavParams} from 'ionic-angular';
+import {STRINGS} from '../../lib/provider/config';
+import {TranslateService} from "@ngx-translate/core";
+import { ChangePasswordPage } from '../change-password/change-password';
+import { Content, LoadingController, ToastController, PopoverController, ModalController } from 'ionic-angular';
+import { PasswordPopover } from '../../popover/password';
+import { ParentVerifyModal } from '../../modals/parent-verify-modal';
+import { PhoneInput } from '../../lib/components/phone-input.component';
+import { AccordionBox } from '../../lib/components/accordion-box';
+import { AlertController } from 'ionic-angular';
+
+@Component({
+  templateUrl: 'mygroups.html'
+})
+
+export class MyGroupsPage {
+
+  @ViewChild(Content) content: Content;
+  
+  public key: string = '';
+  public val: string = '';
+  public errors: Array<string> = [];
+
+  public loadingOverlay;  
+
+  // Constructor
+  constructor(public nav: NavController,
+              public userServices: UserServices,
+              public loadingController: LoadingController,
+              public modalCtrl: ModalController,
+              public alertCtrl: AlertController,
+              public toastController: ToastController,
+              private popoverCtrl: PopoverController) {
+  }
+
+  
+
+    ionViewDidLoad() {
+      console.log("MyGroups: ionViewDidLoad");
+    
+      //let getMyGroupsObservable =  this.userServices.getMyGroups();
+      
+          this.clearErrors();
+          this.cleanBooleans();
+          this.showLoading();
+      
+    }
+
+    ionViewWillEnter() {
+      console.log("MyGroups: ionViewWillEnter");
+    }
+ 
+
+  presentToast(message: string) {
+    let toast = this.toastController.create({
+      message: message,
+      duration: 2000,
+      position: 'middle'
+    });
+    toast.present();
+  }
+  
+  showLoading() {
+    this.loadingOverlay = this.loadingController.create({
+      content: 'Please wait...'
+    });
+    //this.loadingOverlay.present();
+  }
+  
+  hideLoading() {
+    this.loadingOverlay.dismiss();
+  }
+  
+  cleanBooleans() {
+    console.log("cleanBooleans");
+  }
+
+  clearErrors() {
+    console.log("clearErrors");
+    this.errors = [];
+
+  }
+
+  back() {
+    this.nav.popToRoot();
+  }
+
+}
