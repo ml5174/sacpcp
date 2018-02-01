cafd8972cd7cbe8524ab717a4286c87d9bb5f0cb
Shine, Michael (ms8342) <ms8342@us.att.com>
2018-01-31 23:09:59 -0500

    put back to _build_number_ placeholder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -289,7 +289,7 @@ showMyGroups()
       this.storage.set('version', version).then((resource) => {
           console.log('Storing Marketing Version: ' + this.appMarketingVersion);
         });
-      let buildNumberNonMobileFE = "1802010307";
+      let buildNumberNonMobileFE = "_build_number_";
       this.storage.set('build', buildNumberNonMobileFE).then((resource) => {
          console.log('Storing Build Version: ' + buildNumberNonMobileFE);
         });
