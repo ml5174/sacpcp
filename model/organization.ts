969a03de7068ffee15332ea522d4047add1252fb
Shine, Michael (ms8342) <ms8342@us.att.com>
2018-02-03 09:28:14 -0500

    US 9.10 call the correct API for organizationcontacts

--- model/organization.ts
+++ model/organization.ts
@@ -1,9 +1,9 @@
 
-import {Contact} from '../model/contact'
-export class Organization{
-name: string;
-group: string;
-description: string;
-organization_id: string;
-status: number;
+import { Contact } from '../model/contact'
+export class Organization {
+    name: string;
+    group: string;
+    description: string;
+    organization_id: string;
+    status: number;
 }
