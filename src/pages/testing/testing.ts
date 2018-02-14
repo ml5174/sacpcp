dfe797fa3cb85162319949fb531bfef31c060cc9
Dybowski, Dennis (dd8312) <dd8312@us.att.com>
2018-02-14 10:54:13 -0600

    [SAVI - 20180214] began integrating edit-group-attendance with manageAttendee.

--- src/pages/testing/testing.ts
+++ src/pages/testing/testing.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ModalController} from 'ionic-angular';
+import {EditGroupAttendancePage} from '../edit-group-attendance/edit-group-attendance';
 import {GroupAttendeeModal} from '../../modals/group-attendee-modal';
 import {Member} from '../../lib/model/member';
 
@@ -17,6 +18,20 @@ export class TestingPage {
     public modalControl: ModalController) {
     
   }
+  public openEditGroupAttendance() {
+     console.log("openEditGroupAttendance");
+      let attendee = new Member();
+      attendee.first_name = "Peter";
+      attendee.last_name = "Smith";
+      attendee.isAttending = false;
+      attendee.ext_id = "435";
+      attendee.contactString = "barndancer@square.com";
+      let myModal = this.modalControl.create(EditGroupAttendancePage);
+      myModal.onDidDismiss(data => {
+         console.log(data);
+   });      
+   myModal.present();     
+  }
 
   public openBasicModal() {
       console.log("openBasicModal");
@@ -26,7 +41,7 @@ export class TestingPage {
       attendee.isAttending = false;
       attendee.ext_id = "435";
       attendee.contactString = "barndancer@square.com";
-      let myModal = this.modalControl.create(GroupAttendeeModal);
+      let myModal = this.modalControl.create(GroupAttendeeModal, {attendee: attendee});
       myModal.onDidDismiss(data => {
          console.log(data);
    });      
