ba667d03244e6008c3378625b3f77d437034ea70
Dybowski, Dennis (dd8312) <dd8312@us.att.com>
2018-02-18 12:52:17 -0600

    added fields to better accomodate attendees of a group-event.

--- model/member.ts
+++ model/member.ts
@@ -1,4 +1,5 @@
 import { Contact } from '../model/contact'
 export class Member extends Contact {
-    isAttending: boolean; 
+    isAttending: boolean;
+    contactMethod? : number = 0;  //this should be an enum 0none, 1mobile, 2email 
 }
