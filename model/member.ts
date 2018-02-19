4d562b65f33ebf983b2ddf5474a8cb501afdf75d
Dybowski, Dennis (dd8312) <dd8312@us.att.com>
2018-02-19 15:34:01 -0600

    [SAVI-LIB 20180219] Added field isTypeAttendee in order to differentiate between attendees and members of group-event.

--- model/member.ts
+++ model/member.ts
@@ -1,5 +1,6 @@
 import { Contact } from '../model/contact'
 export class Member extends Contact {
     isAttending: boolean;
-    contactMethod? : number = 0;  //this should be an enum 0none, 1mobile, 2email 
+    isTypeAttendee: boolean = false; // used to differentiate between normal group members and attendee for event editing purposes
+    contactMethod? : number = 0;  // TODO: this should be an enum 0=none, 1=mobile, 2=email (for now, hard coded)
 }
