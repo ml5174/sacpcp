c07fe840d8fec6e2d2dddf5e1d96936c46e9381f
Unknown <gonzalez.david995@gmail.com>
2018-08-02 16:02:49 -0500

    Defect 439: Changed about me to my profile, and added correct redirect

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -114,7 +114,7 @@ export class LoginPage {
                       let confirm = this.alertCtrl.create({
                               title: '',
                               cssClass: 'alertReminder',
-                              message: 'You Have not filled in all of the required information to sign up for an event. <br><br> Would you like to navigate to the about me page?',
+                              message: 'You Have not filled in all of the required information to sign up for an event. <br><br> Would you like to navigate to the My Profile page?',
                               buttons: [
                                   {
                                       text: 'No',
