8b8e68f64d298707f251c3872cb2931499c3c793
Anderson, Jude (ja999b) <ja999b@att.com>
2018-09-11 13:11:18 -0400

    [DEF-424] Updating Signup Modal to contain event Info

--- src/pages/events/eventsignup_modal.ts
+++ src/pages/events/eventsignup_modal.ts
@@ -52,6 +52,7 @@ export class EventSignupModal {
     }
 
     ionViewWillEnter() {
+      console.log(this.eventData);
         this.loadData();
         this.eventType = this.eventData.org_restriction != undefined ? this.eventData.org_restriction : this.eventData.eventexpanded.org_restriction;
     }
